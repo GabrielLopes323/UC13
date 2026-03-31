ac7973f7ca5b4620b2e6dcfe8600dae8a8c8b073
kauan <kauan.santos@hotmail.com>
2026-03-31 20:47:21 -0300

    atualizacao do projeto

--- app/src/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Home() {
-  return (
-    <View className="bg-slate-900 flex-1">
-      <Text className="text-white"> Hello React Native!</Text>
-    </View>
-  );
-}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,10 @@
 import { View } from "react-native";
-
 import { Header } from "@/components/header";
 
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Header />
+      <Header title="Faça seu pedido" cartQuantityItems={5} />
     </View>
   );
 }
